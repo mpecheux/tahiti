65640f9c924a6ffb84b832da2617e85e8a79e876
PECHEUX Maxence <maxence.pecheux@soprahr.com>
2026-04-21 16:59:59 +0200

    ortho

--- contenu.js
+++ contenu.js
@@ -5,15 +5,15 @@ var contenu = {
   },
   "partie1": {
     "titre": "Partie 1 – Où se trouve Tahiti ?",
-    "texte": "Tahiti est une île de l’archipel de la société qui fait partie des 5 archipels de la Polynésie Française. \n Tahiti est situé dans le sud de l’Océan Pacifique à environ 15000 km de la France. \n C’est l’île la plus grande et la plus peuplée de la Polynésie Française, sa surface est de 1042 km2 et sa population comporte 192760 habitants. \n La capitale de Tahiti se nomme Papeete"
+    "texte": "Tahiti est une île de l’archipel de la Société qui fait partie des 5 archipels de la Polynésie Française. \n Tahiti est située dans le sud de l’Océan Pacifique à environ 15000 km de la France. \n C’est l’île la plus grande et la plus peuplée de la Polynésie Française, sa surface est de 1042 km2 et sa population comporte 192760 habitants. \n La capitale de Tahiti se nomme Papeete"
   },
   "partie2": {
     "titre": "Partie 2 – La nature et les paysages",
-    "texte": "Tahiti est une île volcanique entourée d’une barrière de corail. Son plus haut sommet est le mont Orohena qui culmine à 2241m de hauteur. Les Tahitiens habitent principalement sur les bords de l’ile. \n Le climat de Tahiti est tropical, c’est-à-dire chaud et humide. \n Sa nature est luxuriante, la fleur blanche emblème de Tahiti est le tiaré. Il y a de nombreuses espèces marines : dauphins, requins, tortue et baleines. La protection de la nature à Tahiti est très importante."
+    "texte": "Tahiti est une île volcanique entourée d’une barrière de corail. Son plus haut sommet est le mont Orohena qui culmine à 2241m de hauteur. Les Tahitiens habitent principalement sur les bords de l’île. \n Le climat de Tahiti est tropical, c’est-à-dire chaud et humide. \n Sa nature est luxuriante, la fleur blanche emblème de Tahiti est le tiaré. Il y a de nombreuses espèces marines : dauphins, requins, tortues et baleines. La protection de la nature à Tahiti est très importante."
   },
   "partie3": {
     "titre": "Partie 3 – La culture et les traditions",
-    "texte": "Les Tahitiens ont hérité de leurs ancêtres les Ma’ohi une culture et des traditions très riches : \n-	La musique et le chant à l’aide du Ukulele\n-	La danse traditionnelle qui s’appelle le Tamouré\n-	De nombreuses pratiques sportives comme le surf\nLes tahitiens parlent le Français et le Tahitien.\nIls sont connus pour leur accueil très chaleureux, leur mot de bienvenu est : Ia Orana."
+    "texte": "Les Tahitiens ont hérité de leurs ancêtres les Ma’ohi une culture et des traditions très riches : \n-	La musique et le chant à l’aide du Ukulele\n-	La danse traditionnelle qui s’appelle le Tamouré\n-	De nombreuses pratiques sportives comme le surf\nLes Tahitiens parlent le Français et le Tahitien.\nIls sont connus pour leur accueil très chaleureux, leur mot de bienvenue est : Ia Orana."
   },
   "quiz": {
     "titre": "Quiz – As-tu bien retenu ?",
@@ -44,7 +44,7 @@ var contenu = {
         "reponse": 2
       },
       {
-        "question": "Quel est le point culminent de Tahiti ?",
+        "question": "Quel est le point culminant de Tahiti ?",
         "choix": ["Le mont Orohena", "Le mont Fuji", "Le mont Blanc"],
         "reponse": 0
       }
@@ -52,6 +52,6 @@ var contenu = {
   },
   "fin": {
     "titre": "Merci !",
-    "credits": "Sources : Wikipédia (https://fr.wikipedia.org/wiki/Tahiti), Tahiti toursime (www.tahititourisme.fr).\nExposé réalisé par : Antoine Pécheux"
+    "credits": "Sources : Wikipédia (https://fr.wikipedia.org/wiki/Tahiti), Tahiti tourisme (www.tahititourisme.fr).\nExposé réalisé par : Antoine Pécheux"
   }
 };
